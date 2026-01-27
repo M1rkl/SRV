c68137b125101dd2dc01ac141bfae36d60e00613
Mirk <78742109+M1rkl@users.noreply.github.com>
2026-01-28 00:31:01 +0200

    Add files via upload

---
+++ laba7/TcpClient/TcpClient.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+    string ipAddress = "127.0.0.1";
+    int port = 54000;
+
+    // Инициализация
+    WSAData data;
+    WORD ver = MAKEWORD(2, 2);
+    int wsResult = WSAStartup(ver, &data);
+    if (wsResult != 0) {
+        cerr << "Can't start Winsock" << endl;
+        return;
+    }
+
+    // Создание сокета TCP
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        cerr << "Can't create socket" << endl;
+        WSACleanup();
+        return;
+    }
+
+    // Заполнение структуры
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+    // Подключение к серверу
+    int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+    if (connResult == SOCKET_ERROR) {
+        cerr << "Can't connect to server" << endl;
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+
+    char buf[4096];
+    string userInput;
+
+    // Цикл отправки
+    do
+    {
+        cout << "> ";
+        getline(cin, userInput);
+
+        if (userInput.size() > 0)
+        {
+            // Отправка
+            int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+            if (sendResult != SOCKET_ERROR)
+            {
+                // Ожидание ответа (Эхо)
+                ZeroMemory(buf, 4096);
+                int bytesReceived = recv(sock, buf, 4096, 0);
+                if (bytesReceived > 0)
+                {
+                    cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+                }
+            }
+        }
+
+        // Условие выхода
+        if (userInput == "quit") {
+            cout << "Quitting..." << endl;
+            break;
+        }
+
+    } while (userInput.size() > 0);
+
+    closesocket(sock);
+    WSACleanup();
+}
---
+++ laba7/UdpClient/UdpClient.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+    WSADATA data;
+    WORD version = MAKEWORD(2, 2);
+    int wsOk = WSAStartup(version, &data);
+    if (wsOk != 0)
+    {
+        cout << "Can't start Winsock! " << wsOk;
+        return;
+    }
+
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(54000);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+
+    SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+
+    cout << "UDP Client ready. Type your message." << endl;
+
+    string msg = "";
+    while (true)
+    {
+        cout << "> ";
+        getline(cin, msg);
+
+        if (msg == "quit") break;
+
+        int sendOk = sendto(out, msg.c_str(), msg.size() + 1, 0, (sockaddr*)&server, sizeof(server));
+
+        if (sendOk == SOCKET_ERROR)
+        {
+            cout << "That didn't work! " << WSAGetLastError() << endl;
+        }
+    }
+
+    closesocket(out);
+    WSACleanup();
+}
